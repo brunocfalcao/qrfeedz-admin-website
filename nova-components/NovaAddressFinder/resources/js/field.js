387c9d31e99eef618b2ecc542978567f28d1afe3
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-30 14:53:00 +0200

    wip

--- nova-components/NovaAddressFinder/resources/js/field.js
+++
@@ -1,9 +0,0 @@
-import IndexField from './components/IndexField'
-import DetailField from './components/DetailField'
-import FormField from './components/FormField'
-
-Nova.booting((app, store) => {
-  app.component('index-nova-address-finder', IndexField)
-  app.component('detail-nova-address-finder', DetailField)
-  app.component('form-nova-address-finder', FormField)
-})
